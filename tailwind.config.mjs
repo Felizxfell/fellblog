400a9a6e1ca225bb6b0e8fc52fead6ed36aa88cd
Felizxfell <felizxhgf@gmail.com>
2024-05-28 12:28:57 -0600

    Migration my blog next to astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,9 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      "tilt-neon": ["TiltNeon", "Arial"],
+    },
     extend: {
       colors: {
         shadow: "#1a1a1a",
